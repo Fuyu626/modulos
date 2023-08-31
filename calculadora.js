f277e836a654b3d34fc919fded903ca5b8f921c3
Fuyu626 <emanuelaveronezdasilva@gmail.com>
2023-08-31 08:46:38 -0300

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,7 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var subtrair = require("./modulos/subtrair");
+var multiplicar = require("./modulos/multi");
 
 console.log(somar(20,90));
 console.log(media(20,90));
