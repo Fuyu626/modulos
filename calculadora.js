263537fc6b2a452f927bf2b51a42b38fdee39642
Fuyu626 <emanuelaveronezdasilva@gmail.com>
2023-08-31 08:49:08 -0300

    calculadoraeh

--- calculadora.js
+++ calculadora.js
@@ -3,5 +3,7 @@ var media = require("./modulos/media");
 var subtrair = require("./modulos/subtrair");
 var multiplicar = require("./modulos/multi");
 
-console.log(somar(20,90));
-console.log(media(20,90));
+console.log("A soma de x e y é: " + somar(20,90));
+console.log("A média de x e y é: " + media(20,90));
+console.log("A subtração de x e y é: " + subtrair(20,90));
+console.log("O produto da multiplicação de x e y é: " + multiplicar(20,90));
